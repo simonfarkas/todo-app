7668c5c28dbe3053f8c648db86bd3c0cc3197d36
simonfarkas <simon.farkas0@gmail.com>
2022-09-25 17:41:56 +0200

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useEffect, useState } from "react";
+import {
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import { TiTick } from "react-icons/ti";
+import { VscClose } from "react-icons/vsc";
 
 function App() {
+  const [todos, setTodos] = useState<any>(() => {
+    // get the todos from localstorage
+    const savedTodos = localStorage.getItem("todos");
+    // if there are todos stored
+    if (savedTodos) {
+      // return the parsed JSON object back to a javascript object
+      return JSON.parse(savedTodos);
+      // otherwise
+    } else {
+      // return an empty array
+      return [];
+    }
+  });
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (input !== "") {
+      const newTodo = {
+        id: new Date().getTime(),
+        name: input,
+        isCompleted: false,
+      };
+      //@ts-ignore
+      setTodos([...todos, newTodo]);
+      setInput("");
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo: any) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const handleComplete = (id: number) => {
+    const newState = todos.map((todo: any) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+
+    setTodos(newState);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Flex direction="column" mt={32}>
+        <Heading mb={10} textAlign="center">
+          Todo App
+        </Heading>
+        <Text>Add title</Text>
+        <form onSubmit={handleSubmit}>
+          <Flex direction="row" experimental_spaceX={2}>
+            <Input
+              placeholder="Clean my room"
+              value={input}
+              onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                setInput(e.target.value)
+              }
+            />
+            <Button colorScheme="green" type="submit">
+              Add
+            </Button>
+          </Flex>
+        </form>
+
+        <Flex direction="column" mt={10}>
+          {todos.map(({ id, name, isCompleted }: any) => (
+            <Flex
+              key={id}
+              direction="row"
+              justify="space-between"
+              align="center"
+              p={4}
+              borderBottom="1px solid #ccc"
+            >
+              <Text textDecoration={isCompleted ? "line-through" : "none"}>
+                {name}
+              </Text>
+              <Flex>
+                {!isCompleted ? (
+                  <TiTick
+                    size={32}
+                    color="green"
+                    onClick={() => handleComplete(id)}
+                  />
+                ) : null}
+                <VscClose
+                  size={32}
+                  color="red"
+                  onClick={() => handleDelete(id)}
+                />
+              </Flex>
+            </Flex>
+          ))}
+        </Flex>
+      </Flex>
+    </Container>
   );
 }
 
